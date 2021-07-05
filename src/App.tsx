4b9c7ebf17ebe286ad3e09140b2e1e8c35b15d39
Bukola Omojowo <bomojowo@gmail.com>
2021-07-05 17:52:49 +0100

    Wrapped return value with ()

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 import { greet } from "./utils/greet";
+// import episodes from './episodes.json'
+import Episodes from './components/Episodes'
 
 function App(): JSX.Element {
-  return <h1>{greet("World")}</h1>;
+  return (
+  <div>
+    <h1>{greet("World")}</h1>
+    <Episodes />
+  </div>
+  )
 }
 
 export default App;
+
+// console.log(`Imported ${episodes.length} episode(s)`);
+// console.log(`First episode's name is ${episodes[0].name}`);
