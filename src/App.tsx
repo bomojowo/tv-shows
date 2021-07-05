295068afe7fb11172d8be82630747ba7ba4c49e5
Bukola Omojowo <bomojowo@gmail.com>
2021-07-05 17:53:59 +0100

    Formatted using prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import { greet } from "./utils/greet";
 // import episodes from './episodes.json'
-import Episodes from './components/Episodes'
+import Episodes from "./components/Episodes";
 
 function App(): JSX.Element {
   return (
-  <div>
-    <h1>{greet("World")}</h1>
-    <Episodes />
-  </div>
-  )
+    <div>
+      <h1>{greet("World")}</h1>
+      <Episodes />
+    </div>
+  );
 }
 
 export default App;
