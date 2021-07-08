102801d7f0282620e1424c9fb65e30459d952869
Bukola Omojowo <bomojowo@gmail.com>
2021-07-08 12:54:37 +0100

    Formatted using prettier

--- src/components/Episodes.tsx
+++ src/components/Episodes.tsx
@@ -1,7 +1,7 @@
 import episodes from "../episodes.json";
 import "./Episodes.css";
 import { Card } from "react-bootstrap";
-import {useState} from "react";
+import { useState } from "react";
 
 interface IEpisode {
   id: number;
@@ -23,41 +23,54 @@ interface IEpisode {
 }
 
 function Episodes(): JSX.Element {
-  const [search, setSearch] = useState('')
+  const [search, setSearch] = useState("");
   return (
     <div>
       <h1>TV Shows</h1>
-      <input className= "searchBar" type="text" placeholder="Search..." onChange={(e) => {setSearch(e.target.value)}} />
-  
+      <input
+        className="searchBar"
+        type="text"
+        placeholder="Search..."
+        onChange={(e) => {
+          setSearch(e.target.value);
+        }}
+      />
+
       <div className="card-container">
-        {episodes.filter((filteredEp: IEpisode) => {
-          if (search === ''){
-            return filteredEp
-          } else if (filteredEp.summary.toLocaleLowerCase().includes(search) || filteredEp.name.toLocaleLowerCase().includes(search)){
-            return filteredEp
-          }
-        }).map((episode: IEpisode) => (
-          <div key={episode.id}>
-            <Card className="card" style={{ width: "30rem", height: "35em" }}>
-              <Card.Title className="card-title">
-                <b>
-                  {episode.name} - S{String(episode.season).padStart(2, "0")} E0
-                  {String(episode.number).padStart(1, "0")}
-                </b>
-              </Card.Title>
-              <Card.Img
-                className="card-img-top"
-                variant="top"
-                src={episode.image.medium}
-                alt="scene from episode"
-              />
-              <Card.Body className="card-block">
-                <Card.Text>{episode.summary}</Card.Text>
-                {/* <Button variant="primary">TMZase</Button> */}
-              </Card.Body>
-            </Card>
-          </div>
-        ))}
+        {episodes
+          .filter((filteredEp: IEpisode) => {
+            if (search === "") {
+              return filteredEp;
+            } else if (
+              filteredEp.summary.toLocaleLowerCase().includes(search) ||
+              filteredEp.name.toLocaleLowerCase().includes(search)
+            ) {
+              return filteredEp;
+            }
+          })
+          .map((episode: IEpisode) => (
+            <div key={episode.id}>
+              <Card className="card" style={{ width: "30rem", height: "35em" }}>
+                <Card.Title className="card-title">
+                  <b>
+                    {episode.name} - S{String(episode.season).padStart(2, "0")}{" "}
+                    E0
+                    {String(episode.number).padStart(1, "0")}
+                  </b>
+                </Card.Title>
+                <Card.Img
+                  className="card-img-top"
+                  variant="top"
+                  src={episode.image.medium}
+                  alt="scene from episode"
+                />
+                <Card.Body className="card-block">
+                  <Card.Text>{episode.summary}</Card.Text>
+                  {/* <Button variant="primary">TMZase</Button> */}
+                </Card.Body>
+              </Card>
+            </div>
+          ))}
       </div>
       <hr />
       <p>
