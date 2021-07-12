992328c9af25da6dc0b95a8180f1c865afa1e224
Bukola Omojowo <bomojowo@gmail.com>
2021-07-12 12:32:35 +0100

    Added search component and created search list

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,16 +1,18 @@
+import "./Search.css";
+
 interface SearchProps {
   setSearch: React.Dispatch<React.SetStateAction<string>>;
 }
 
-function Search(props: SearchProps): JSX.Element {
+function Search({setSearch}: SearchProps): JSX.Element {
   return (
     <div>
       <input
-        className="searchBar"
+      className="searchBar"
         type="text"
         placeholder="Search..."
         onChange={(e) => {
-          props.setSearch(e.target.value);
+        setSearch(e.target.value);
         }}
       />
     </div>
