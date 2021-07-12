e800992bbc8170d6306a5c6489a00270944e0ae0
Bukola Omojowo <bomojowo@gmail.com>
2021-07-12 12:33:55 +0100

    created select feature

--- src/components/Select.tsx
+++ src/components/Select.tsx
@@ -1,30 +1,22 @@
 import episodes from "../episodes.json"
-import DropdownButton from "react-bootstrap/DropdownButton";
-import Dropdown from "react-bootstrap/Dropdown";
+
 
 
 
 function Select(): JSX.Element{
+
     return(
         <div>
-            <DropdownButton
-                className="drop-down-list"
-                id="dropdown-basic-button"
-                title="Select"
-      >
-        {episodes.map((title) => (
-          <div key={title.id}>
-            <Dropdown.Item href="#/action-1">
-              <div key={title.id}>
-                <li>{title.name}</li>
-              </div>
-            </Dropdown.Item>
-          </div>
-        ))}
-            </DropdownButton>
-
+          <select>
+            
+            {episodes.map((title) =>
+            
+            <option value={title.name} key={title.id}>{title.name}</option>
+            )}
+          </select>
         </div>
     )
 }
 
 export default Select;
+
