45013041482318577cdd3ca295671fb1d26452e8
Bukola Omojowo <bomojowo@gmail.com>
2021-07-13 15:50:28 +0100

    created function with event handler and css

--- src/components/Select.tsx
+++ src/components/Select.tsx
@@ -1,12 +1,48 @@
-import episodes from "../episodes.json";
 
-function Select(): JSX.Element {
+import episodes from "../episodes.json";
+import "./Select.css"
+
+
+interface IEpisode {
+  id: number;
+  url: string;
+  name: string;
+  season: number;
+  number: number;
+  type: string;
+  airdate: string;
+  airtime: string;
+  airstamp: string;
+  runtime: number;
+  image: {
+    medium: string;
+    original: string;
+  };
+  summary: string;
+  _links: { self: { href: string } };
+}
+
+interface SelectedProps {
+  setSelectedEp: React.Dispatch<React.SetStateAction<string>>;
+}
+
+function Select({setSelectedEp}: SelectedProps): JSX.Element {
+  function handleEpisodeSelectorChange(event: any): void{
+    // setSelectedEp(selectedEp )
+        console.log('working')
+        //console.log(event.target.value)
+        setSelectedEp(event.target.value)
+    
+    }
   return (
     <div>
-      <select>
-        {episodes.map((title) => (
-          <option value={title.name} key={title.id}>
-            {title.name}
+      <select className="select-bar" onChange={(handleEpisodeSelectorChange)}
+      >
+        <option value="">Select an Episode</option>
+        {episodes.map((episode: IEpisode) => (
+          
+          <option value={episode.name} key={episode.id}>
+            {episode.name} ({episode.id})
           </option>
         ))}
       </select>
